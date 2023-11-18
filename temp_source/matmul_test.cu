c8f32aee06198eaf4d629a582316131e25017616
zhaoruic <czhaor@gmail.com>
2023-11-18 16:44:24 +0800

    optimize mm.

--- temp_source/matmul_test.cu
+++ temp_source/matmul_test.cu
@@ -4,8 +4,28 @@
 #include <memory>
 #include <chrono>
 #include <math.h>
+#include <string>
 
 #define BLOCK_SIZE 16
+
+struct CUProfiler {
+  CUProfiler(std::string name) : name(name) {
+    cudaEventCreate(&start);
+    cudaEventCreate(&end);
+    cudaEventRecord(start);
+  }
+
+  ~CUProfiler() {
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+    cudaEventElapsedTime(&msec, start, end);
+    std::cout << "[" << name << "]: " << "Average elasped time: " << msec << " ms." << std::endl;
+  }
+  std::string name;
+  float msec;
+  cudaEvent_t start;
+  cudaEvent_t end;
+};
 
 void fullfill_rand(float* input, int nelm) {
   std::srand(std::time(nullptr)); // use current time as seed for random generator
@@ -89,12 +109,59 @@ __global__ void matmulShared(float* A, float* B, float* C, int M, int N, int K) 
   }
 }
 
+__global__ void matmulBankOpt(float* A, float* B, float* C, int M, int N, int K) {
+  int i = threadIdx.y + blockIdx.y * blockDim.y;
+  int j = threadIdx.x + blockIdx.x * blockDim.x;
+  int bi = threadIdx.x;
+  int bj = threadIdx.y;
+  
+  if (i < M && j < N) {
+    float sum = 0;
+    __shared__ float shareA[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float shareB[BLOCK_SIZE][BLOCK_SIZE];
+    for (int k = 0; k < K; k+=BLOCK_SIZE) {
+      shareA[bj][bi] = A[i * M + k];
+      shareB[bi][bj] = B[k * N + j];
+      __syncthreads();
+      for (int inner = 0; inner < BLOCK_SIZE; ++inner) {
+        sum += shareA[inner][bi] * shareB[inner][bj];
+      }
+      __syncthreads();
+    }
+    C[i * M + j] = sum;
+  }
+}
+
+__global__ void matmulBankOpt2(float* A, float* B, float* C, int M, int N, int K) {
+  int i = threadIdx.y + blockIdx.y * blockDim.y;
+  int j = threadIdx.x + blockIdx.x * blockDim.x;
+  int bi = threadIdx.x;
+  int bj = threadIdx.y;
+  
+  if (i < M && j < N) {
+    float sum = 0;
+    __shared__ float shareA[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float shareB[BLOCK_SIZE][BLOCK_SIZE + 1];
+    for (int k = 0; k < K; k+=BLOCK_SIZE) {
+      shareA[bj][bi] = A[i * M + k];
+      shareB[bi][bj] = B[k * N + j];
+      __syncthreads();
+      for (int inner = 0; inner < BLOCK_SIZE; ++inner) {
+        sum += shareA[inner][bi] * shareB[inner][bj];
+      }
+      __syncthreads();
+    }
+    C[i * M + j] = sum;
+  }
+}
+
 int main() {
   std::cout << "test matmul naive" << std::endl;
   int M = 1024;
   int N = 3000;
   int K = 2000;
-  const bool checkResult = true;
+  const bool checkResult = false;
+  const int iteration = 50;
   
   float* hA = static_cast<float*>(malloc(M * K * sizeof(float)));
   float* hB = static_cast<float*>(malloc(K * N * sizeof(float)));
@@ -112,7 +179,27 @@ int main() {
   
   dim3 threadPerBlock(BLOCK_SIZE, BLOCK_SIZE);
   dim3 blockPerGrid((M + BLOCK_SIZE - 1) / BLOCK_SIZE, (N + BLOCK_SIZE - 1) / BLOCK_SIZE);
-  matmulShared<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  // matmulBankOpt2<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  // warm up
+  for (int i = 0; i < 20; ++i) {
+    matmulNaive<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  }
+  {
+    CUProfiler profiler("naive");
+    for (int i = 0; i < iteration; ++i) matmulNaive<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  }
+  {
+    CUProfiler profiler("shared mem");
+    for (int i = 0; i < iteration; ++i) matmulShared<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  }
+  {
+    CUProfiler profiler("bank opt");
+    for (int i = 0; i < iteration; ++i) matmulBankOpt<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  }
+  {
+    CUProfiler profiler("bank opt2");
+    for (int i = 0; i < iteration; ++i) matmulBankOpt2<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+  }
 
   cudaMemcpy(hC, dC, M * N * sizeof(float), cudaMemcpyDeviceToHost);
   if (checkResult) {
