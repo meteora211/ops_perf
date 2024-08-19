4dfc5e922b6e20364811cf67b30ca8184e6c9cd8
zhaorui <czhaor@gmail.com>
2024-08-19 07:49:25 +0000

    clean code.

--- cmake/CompileCUDA.cmake
+++ cmake/CompileCUDA.cmake
@@ -3,9 +3,10 @@ function(CompileCUDA SOURCE_FILES)
   foreach(source_file ${SOURCE_FILES})
     get_filename_component(executable_name ${source_file} NAME_WE)
     add_executable(${executable_name} ${source_file})
-    set_target_properties(${executable_name} PROPERTIES
-        CUDA_SEPARABLE_COMPILATION ON
-    )
+    # XXX: CUDA_SEPARABLE_COMPILATION cause significant perf drawback
+    # set_target_properties(${executable_name} PROPERTIES
+    #     CUDA_SEPARABLE_COMPILATION ON
+    # )
     # target_compile_options(${executable_name} PRIVATE -O3 -Wall -fno-elide-constructors)
     install(TARGETS ${executable_name} DESTINATION ${MET_INSTALL_BIN_DIR})
   endforeach()
--- cuda_test/matmul_test.cu
+++ cuda_test/matmul_test.cu
@@ -1,18 +1,33 @@
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include <math.h>
 #include <vector>
 
+#include <sys/time.h>
+
 #include "utils.h"
-#define CEIL_DIV(M, N) (((M) + (N)-1) / (N))
+#define CEIL_DIV(M, N) (((M) + (N) - 1) / (N))
+#define cudaCheck(err) (cudaCheck(err, __FILE__, __LINE__))
 
 // #define BLOCK_SIZE 32
 constexpr int BLOCK_SIZE = 32;
 constexpr int COARSE_FACTOR = 4;
 
+void randomize_matrix(float *mat, int N) {
+  // NOTICE: Use gettimeofday instead of srand((unsigned)time(NULL)); the time
+  // precision is too low and the same random number is generated.
+  struct timeval time {};
+  gettimeofday(&time, nullptr);
+  srand(time.tv_usec);
+  for (int i = 0; i < N; i++) {
+    float tmp = (float)(rand() % 5) + 0.01 * (rand() % 5);
+    tmp = (rand() % 2 == 0) ? tmp : tmp * (-1.);
+    mat[i] = tmp;
+  }
+}
 
-void matmulBase(float* lhs, float* rhs, float* res, int M, int N, int K) {
+void matmulBase(float *lhs, float *rhs, float *res, int M, int N, int K) {
   // lhs(M*K) * rhs(K*N) = res(M*N)
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
@@ -26,10 +41,10 @@ void matmulBase(float* lhs, float* rhs, float* res, int M, int N, int K) {
   }
 }
 
-__global__ void matmulNaive(float* A, float* B, float* C, int M, int N, int K) {
+__global__ void matmulNaive(float *A, float *B, float *C, int M, int N, int K) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
-  
+
   if (i < M && j < N) {
     float sum = 0;
     for (int k = 0; k < K; ++k) {
@@ -39,10 +54,11 @@ __global__ void matmulNaive(float* A, float* B, float* C, int M, int N, int K) {
   }
 }
 
-__global__ void matmulGlobalCoalesc(float* A, float* B, float* C, int M, int N, int K) {
+__global__ void matmulGlobalCoalesc(float *A, float *B, float *C, int M, int N,
+                                    int K) {
   int i = threadIdx.y + blockIdx.y * blockDim.y;
   int j = threadIdx.x + blockIdx.x * blockDim.x;
-  
+
   if (i < M && j < N) {
     float sum = 0;
     for (int k = 0; k < K; ++k) {
@@ -52,16 +68,17 @@ __global__ void matmulGlobalCoalesc(float* A, float* B, float* C, int M, int N, 
   }
 }
 
-__global__ void matmulShared(float* A, float* B, float* C, int M, int N, int K) {
+__global__ void matmulShared(float *A, float *B, float *C, int M, int N,
+                             int K) {
   int i = threadIdx.y + blockIdx.y * blockDim.y;
   int j = threadIdx.x + blockIdx.x * blockDim.x;
   int bi = threadIdx.y;
   int bj = threadIdx.x;
-  
+
   float sum = 0;
   __shared__ float shareA[BLOCK_SIZE][BLOCK_SIZE];
   __shared__ float shareB[BLOCK_SIZE][BLOCK_SIZE];
-  for (int k = 0; k < K; k+=BLOCK_SIZE) {
+  for (int k = 0; k < K; k += BLOCK_SIZE) {
     if ((i < M) && ((k + bj) < K)) {
       shareA[bi][bj] = A[i * K + (k + bj)];
     } else {
@@ -83,16 +100,17 @@ __global__ void matmulShared(float* A, float* B, float* C, int M, int N, int K) 
   }
 }
 
-__global__ void matmulBankOpt(float* A, float* B, float* C, int M, int N, int K) {
+__global__ void matmulBankOpt(float *A, float *B, float *C, int M, int N,
+                              int K) {
   int i = threadIdx.y + blockIdx.y * blockDim.y;
   int j = threadIdx.x + blockIdx.x * blockDim.x;
   int bi = threadIdx.y;
   int bj = threadIdx.x;
-  
+
   float sum = 0;
   __shared__ float shareA[BLOCK_SIZE][BLOCK_SIZE];
   __shared__ float shareB[BLOCK_SIZE][BLOCK_SIZE + 1];
-  for (int k = 0; k < K; k+=BLOCK_SIZE) {
+  for (int k = 0; k < K; k += BLOCK_SIZE) {
     if ((i < M) && ((k + bj) < K)) {
       shareA[bi][bj] = A[i * K + (k + bj)];
     } else {
@@ -114,19 +132,20 @@ __global__ void matmulBankOpt(float* A, float* B, float* C, int M, int N, int K)
   }
 }
 
-__global__ void matmulThreadCoarsening(float* A, float* B, float* C, int M, int N, int K) {
+__global__ void matmulThreadCoarsening(float *A, float *B, float *C, int M,
+                                       int N, int K) {
   int i = threadIdx.y + blockIdx.y * blockDim.y;
   int j = threadIdx.x + blockIdx.x * blockDim.x * COARSE_FACTOR;
   int bi = threadIdx.y;
   int bj = threadIdx.x;
-  
+
   float value[COARSE_FACTOR];
   for (int c = 0; c < COARSE_FACTOR; ++c) {
     value[c] = 0.0;
   }
   __shared__ float shareA[BLOCK_SIZE][BLOCK_SIZE + 1];
   __shared__ float shareB[BLOCK_SIZE][BLOCK_SIZE + 1];
-  for (int k = 0; k < K; k+=BLOCK_SIZE) {
+  for (int k = 0; k < K; k += BLOCK_SIZE) {
     if ((i < M) && ((k + bj) < K)) {
       shareA[bi][bj] = A[i * K + (k + bj)];
     } else {
@@ -155,7 +174,8 @@ __global__ void matmulThreadCoarsening(float* A, float* B, float* C, int M, int 
 }
 
 // template <const int BM, const int BN, const int BK, const int TM>
-// __global__ void matmul1DBlockTiling(float *A, float *B, float *C, int M, int N, int K) {
+// __global__ void matmul1DBlockTiling(float *A, float *B, float *C, int M, int
+// N, int K) {
 //   int i = threadIdx.y + blockIdx.y * blockDim.y;
 //   int j = threadIdx.x + blockIdx.x * blockDim.x;
 //   int ti = threadIdx.y;
@@ -188,196 +208,45 @@ __global__ void matmulThreadCoarsening(float* A, float* B, float* C, int M, int 
 //   }
 // }
 
-// template <const int BM, const int BN, const int BK, const int TM>
-// __global__ void matmul1DBlockTiling(float *A, float *B, float *C, int M, int N, int K) {
-//   const uint bRow = blockIdx.y;
-//   const uint bCol = blockIdx.x;
-//
-//   __shared__ float shareA[BM * BK];
-//   __shared__ float shareB[BK * BN];
-//
-//   A += bRow * BM * K;
-//   B += bCol * BN;
-//   C += bRow * BM * N + bCol * BN;
-//
-//   float threadRes[TM] = {0.0};
-//
-//   const uint rowA = threadIdx.x / BK;
-//   const uint colA = threadIdx.x % BK;
-//   const uint rowB = threadIdx.x / BN;
-//   const uint colB = threadIdx.x % BN;
-//   const int threadRow = threadIdx.x / BN;
-//   const int threadCol = threadIdx.x % BN;
-//
-//   for (int k = 0; k < K; k+=BK) {
-//     shareA[rowA * BK + colA] = A[rowA * K + (colA + k)];
-//     shareB[rowB * BN + colB] = B[(rowB + k) * N + colB];
-//     __syncthreads();
-//     for (int tid = 0; tid < TM; ++tid) {
-//       for (int inner = 0; inner < BK; ++inner) {
-//         threadRes[tid] += shareA[(threadRow * TM + tid) * BK + inner] * shareB[inner * BN + threadCol];
-//       }
-//     }
-//     __syncthreads();
-//   }
-//
-//   for (int tid = 0; tid < TM; ++tid) {
-//     C[(threadRow * TM + tid) * N + threadCol] = threadRes[tid];
-//   }
-// }
-
-__global__ void mysgemm_v2(float *A, float *B, float *C, int M, int N, int K) {
-  // int i = threadIdx.y + blockIdx.y * blockDim.y;
-  // int j = threadIdx.x + blockIdx.x * blockDim.x;
-  int bj = blockIdx.x;
-  int bi = blockIdx.y;
-
-  const int BM = BLOCK_SIZE;
-  const int BN = BLOCK_SIZE;
-  const int BK = BLOCK_SIZE;
-  
-  int tx = threadIdx.x % BN;
-  int ty = threadIdx.x / BN;
-
-  __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
-  __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
-
-  // 移动到当前block
-  A = &A[bi * BM * K];
-  B = &B[bj * BN];
-  C = &C[bi * BM * N + bj * BN];
-
-  float tmp = 0.;
-  for (int k = 0; k < K; k += BK) {
-      // 缓存A_tile和B_tile
-      As[ty][tx] = A[ty * K + tx];
-      Bs[ty][tx] = B[ty * N + tx];
-      // 同步所有线程缓存完成
-      __syncthreads();
-      A += BK;
-      B += BK * N;
-      for (int i = 0; i < BK; i++) {
-          tmp += As[ty][i] * Bs[i][tx];
-      }
-      // FMA计算需要读取缓存数据，在新一轮写入缓存前进行同步，确保所有线程计算完成
-      __syncthreads();
-  }
-  C[ty * N + tx] = tmp + C[ty * N + tx];
-}
-
-__global__ void sgemm_shared_mem_block(const float *A, const float *B,float *C,
-                                       int M, int N, int K) {
-  // the output block that we want to compute in this threadblock
-  const uint cRow = blockIdx.x;
-  const uint cCol = blockIdx.y;
-
-  // allocate buffer for current block in fast shared mem
-  // shared mem is shared between all threads in a block
-  __shared__ float As[BLOCK_SIZE * BLOCK_SIZE];
-  __shared__ float Bs[BLOCK_SIZE * BLOCK_SIZE];
-
-  // the inner row & col that we're accessing in this thread
-  const uint threadCol = threadIdx.x % BLOCK_SIZE;
-  const uint threadRow = threadIdx.x / BLOCK_SIZE;
-
-  // advance pointers to the starting positions
-  A += cRow * BLOCK_SIZE * K;                    // row=cRow, col=0
-  B += cCol * BLOCK_SIZE;                        // row=0, col=cCol
-  C += cRow * BLOCK_SIZE * N + cCol * BLOCK_SIZE; // row=cRow, col=cCol
-
-  float tmp = 0.0;
-  for (int bkIdx = 0; bkIdx < K; bkIdx += BLOCK_SIZE) {
-    // Have each thread load one of the elements in A & B
-    // Make the threadCol (=threadIdx.x) the consecutive index
-    // to allow global memory access coalescing
-    As[threadRow * BLOCK_SIZE + threadCol] = A[threadRow * K + threadCol];
-    Bs[threadRow * BLOCK_SIZE + threadCol] = B[threadRow * N + threadCol];
-
-    // block threads in this block until cache is fully populated
-    __syncthreads();
-    A += BLOCK_SIZE;
-    B += BLOCK_SIZE * N;
-
-    // execute the dotproduct on the currently cached block
-    for (int dotIdx = 0; dotIdx < BLOCK_SIZE; ++dotIdx) {
-      tmp += As[threadRow * BLOCK_SIZE + dotIdx] *
-             Bs[dotIdx * BLOCK_SIZE + threadCol];
-    }
-    // need to sync again at the end, to avoid faster threads
-    // fetching the next block into the cache before slower threads are done
-    __syncthreads();
-  }
-  C[threadRow * N + threadCol] =
-      tmp + C[threadRow * N + threadCol];
-}
-
 template <const int BM, const int BN, const int BK, const int TM>
-__global__ void matmul1DBlockTiling(float *A, float *B, float *C, int M, int N, int K) {
-  // If we flip x and y here we get ~30% less performance for large matrices.
-  // The current, 30% faster configuration ensures that blocks with sequential
-  // blockIDs access columns of B sequentially, while sharing the same row of A.
-  // The slower configuration would share columns of A, but access into B would
-  // be non-sequential. So the faster configuration has better spatial locality
-  // and hence a greater L2 hit rate.
-  const uint cRow = blockIdx.y;
-  const uint cCol = blockIdx.x;
+__global__ void matmul1DBlockTiling(float *A, float *B, float *C, int M, int N,
+                                    int K) {
+  const uint bRow = blockIdx.y;
+  const uint bCol = blockIdx.x;
 
-  // each warp will calculate 32*TM elements, with 32 being the columnar dim.
-  const int threadCol = threadIdx.x % BN;
+  __shared__ float shareA[BM * BK];
+  __shared__ float shareB[BK * BN];
+
+  A += bRow * BM * K;
+  B += bCol * BN;
+  C += bRow * BM * N + bCol * BN;
+
+  float threadRes[TM] = {0.0};
+
+  const uint rowA = threadIdx.x / BK;
+  const uint colA = threadIdx.x % BK;
+  const uint rowB = threadIdx.x / BN;
+  const uint colB = threadIdx.x % BN;
   const int threadRow = threadIdx.x / BN;
+  const int threadCol = threadIdx.x % BN;
 
-  // allocate space for the current blocktile in SMEM
-  __shared__ float As[BM * BK];
-  __shared__ float Bs[BK * BN];
-
-  // Move blocktile to beginning of A's row and B's column
-  A += cRow * BM * K;
-  B += cCol * BN;
-  C += cRow * BM * N + cCol * BN;
-
-  // todo: adjust this to each thread to load multiple entries and
-  // better exploit the cache sizes
-  assert(BM * BK == blockDim.x);
-  assert(BN * BK == blockDim.x);
-  const uint innerColA = threadIdx.x % BK; // warp-level GMEM coalescing
-  const uint innerRowA = threadIdx.x / BK;
-  const uint innerColB = threadIdx.x % BN; // warp-level GMEM coalescing
-  const uint innerRowB = threadIdx.x / BN;
-
-  // allocate thread-local cache for results in registerfile
-  float threadResults[TM] = {0.0};
-
-  // outer loop over block tiles
-  for (uint bkIdx = 0; bkIdx < K; bkIdx += BK) {
-    // populate the SMEM caches
-    As[innerRowA * BK + innerColA] = A[innerRowA * K + innerColA];
-    Bs[innerRowB * BN + innerColB] = B[innerRowB * N + innerColB];
+  for (int k = 0; k < K; k += BK) {
+    shareA[rowA * BK + colA] = A[rowA * K + (colA + k)];
+    shareB[rowB * BN + colB] = B[(rowB + k) * N + colB];
     __syncthreads();
-
-    // advance blocktile
-    A += BK;
-    B += BK * N;
-
-    // calculate per-thread results
-    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) {
-      // we make the dotproduct loop the outside loop, which facilitates
-      // reuse of the Bs entry, which we can cache in a tmp var.
-      float tmpB = Bs[dotIdx * BN + threadCol];
-      for (uint resIdx = 0; resIdx < TM; ++resIdx) {
-        threadResults[resIdx] +=
-            As[(threadRow * TM + resIdx) * BK + dotIdx] * tmpB;
+    for (int tid = 0; tid < TM; ++tid) {
+      for (int inner = 0; inner < BK; ++inner) {
+        threadRes[tid] += shareA[(threadRow * TM + tid) * BK + inner] *
+                          shareB[inner * BN + threadCol];
       }
     }
     __syncthreads();
   }
 
-  // write out the results
-  for (uint resIdx = 0; resIdx < TM; ++resIdx) {
-    C[(threadRow * TM + resIdx) * N + threadCol] =
-        threadResults[resIdx];
+  for (int tid = 0; tid < TM; ++tid) {
+    C[(threadRow * TM + tid) * N + threadCol] = threadRes[tid];
   }
 }
-
 
 int main() {
   const bool checkResult = false;
@@ -388,54 +257,56 @@ int main() {
   CudaDeviceInfo();
 
   long M, N, K;
-  // std::cout << M << " " << N << " " << K << " " << flops << " " << sizeof(long) << std::endl;
-  // warm up
-  // for (int i = 0; i < 20; ++i) {
-  //   matmulNaive<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, 1024, 1024, 1024);
-  // }
-  for (const auto& size : SIZES) {
+  for (const auto &size : SIZES) {
     M = N = K = size;
     long flops = 2 * M * N * K;
     dim3 threadPerBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 blockPerGrid((N + BLOCK_SIZE - 1) / BLOCK_SIZE, (M + BLOCK_SIZE - 1) / BLOCK_SIZE);
-    float* hA = static_cast<float*>(malloc(M * K * sizeof(float)));
-    float* hB = static_cast<float*>(malloc(K * N * sizeof(float)));
-    float* hC = static_cast<float*>(malloc(M * N * sizeof(float)));
+    dim3 blockPerGrid((N + BLOCK_SIZE - 1) / BLOCK_SIZE,
+                      (M + BLOCK_SIZE - 1) / BLOCK_SIZE);
+    float *hA = static_cast<float *>(malloc(M * K * sizeof(float)));
+    float *hB = static_cast<float *>(malloc(K * N * sizeof(float)));
+    float *hC = static_cast<float *>(malloc(M * N * sizeof(float)));
     fullfill_rand(hA, M * K);
     fullfill_rand(hB, K * N);
-    
-    float* dA;
+
+    float *dA;
     cudaMalloc(&dA, M * K * sizeof(float));
-    float* dB;
+    float *dB;
     cudaMalloc(&dB, K * N * sizeof(float));
-    float* dC;
+    float *dC;
     cudaMalloc(&dC, M * N * sizeof(float));
-    
+
     cudaMemcpy(dA, hA, M * K * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(dB, hB, K * N * sizeof(float), cudaMemcpyHostToDevice);
-  
+
     // {
     //   CUProfiler profiler("naive", iteration, flops);
-    //   for (int i = 0; i < iteration; ++i) matmulNaive<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-    // }
-    // {
-    //   CUProfiler profiler("global coalescing", iteration, flops);
-    //   for (int i = 0; i < iteration; ++i) matmulGlobalCoalesc<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-    // }
-    // {
-    //   CUProfiler profiler("shared mem", iteration, flops);
-    //   for (int i = 0; i < iteration; ++i) matmulShared<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-    // }
-    // {
-    //   CUProfiler profiler("bank opt", iteration, flops);
-    //   for (int i = 0; i < iteration; ++i) matmulBankOpt<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-    // }
-    // {
-    //   CUProfiler profiler("thread coarsening", iteration, flops);
-    //   for (int i = 0; i < iteration; ++i) matmulThreadCoarsening<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+    //   for (int i = 0; i < iteration; ++i)
+    //     matmulNaive<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
     // }
     {
-      CUProfiler profiler("1d tiling", iteration, flops);
+      CUProfiler profiler("global coalescing", iteration, flops);
+      for (int i = 0; i < iteration; ++i)
+        matmulGlobalCoalesc<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N,
+                                                              K);
+    }
+    {
+      CUProfiler profiler("shared mem", iteration, flops);
+      for (int i = 0; i < iteration; ++i)
+        matmulShared<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+    }
+    {
+      CUProfiler profiler("bank opt", iteration, flops);
+      for (int i = 0; i < iteration; ++i)
+        matmulBankOpt<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+    }
+    {
+      CUProfiler profiler("thread coarsening", iteration, flops);
+      for (int i = 0; i < iteration; ++i)
+        matmulThreadCoarsening<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M,
+                                                                 N, K);
+    }
+    {
 
       float elapsed_time;
       cudaEvent_t beg, end;
@@ -448,35 +319,18 @@ int main() {
       dim3 threadPerBlock(BN * BM / TM);
       // dim3 blockPerGrid((N + BN - 1) / BN, (M + BM - 1) / BM);
       dim3 blockPerGrid(CEIL_DIV(N, BN), CEIL_DIV(M, BM));
-      matmul1DBlockTiling<BM, BN, BK, TM><<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-      cudaEventRecord(beg);
-      for (int i = 0; i < iteration; ++i) matmul1DBlockTiling<BM, BN, BK, TM><<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-      cudaEventRecord(end);
-      cudaEventSynchronize(beg);
-      cudaEventSynchronize(end);
-      cudaEventElapsedTime(&elapsed_time, beg, end);
-      elapsed_time /= 1000.; // Convert to seconds
-
-      printf(
-          "Average elapsed time: (%7.6f) s, performance: (%7.1f) GFLOPS. size: "
-          "(%ld).\n",
-          elapsed_time / iteration,
-          (iteration * flops * 1e-9) / elapsed_time, M);
+      matmul1DBlockTiling<BM, BN, BK, TM>
+          <<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
+      CUProfiler profiler("1d tiling", iteration, flops);
+      for (int i = 0; i < iteration; ++i)
+        matmul1DBlockTiling<BM, BN, BK, TM>
+            <<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
     }
-    // matmulBankOpt<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
     cudaMemcpy(hC, dC, M * N * sizeof(float), cudaMemcpyDeviceToHost);
     if (checkResult) {
-      float* hRef = static_cast<float*>(malloc(M * N * sizeof(float)));
+      float *hRef = static_cast<float *>(malloc(M * N * sizeof(float)));
       matmulBase(hA, hB, hRef, M, N, K);
       matrixChecker(hRef, hC, M, N);
     }
   }
-  // {
-  //   CUProfiler profiler("1111", flops * iteration);
-  //   cudaFuncSetAttribute(sgemm_shared_mem_block,
-  //                        cudaFuncAttributePreferredSharedMemoryCarveout,
-  //                        cudaSharedmemCarveoutMaxShared);
-  //   for (int i = 0; i < iteration; ++i) sgemm_shared_mem_block<<<blockPerGrid, threadPerBlock>>>(dA, dB, dC, M, N, K);
-  // }
-
 }
